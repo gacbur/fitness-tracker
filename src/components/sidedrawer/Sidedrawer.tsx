328a2c728d532ad0cb7125d2d19aadc80c1fcc2b
Kacper <kacper0775@gmail.com>
2021-02-25 16:03:07 +0100

    add sidedrawer routes and functionality

--- src/components/sidedrawer/Sidedrawer.tsx
+++ src/components/sidedrawer/Sidedrawer.tsx
@@ -1,8 +1,10 @@
-import React, { FC, SetStateAction } from 'react'
+import React, { FC, SetStateAction, useState } from 'react'
+
+import { Link } from 'react-router-dom'
 
 import './Sidedrawer.css'
 
-import { IoIosClose } from 'react-icons/io'
+import { IoIosClose, IoIosArrowDown } from 'react-icons/io'
 
 type siderdrawerProps = {
     show: boolean,
@@ -10,6 +12,9 @@ type siderdrawerProps = {
 }
 
 const Sidedrawer: FC<siderdrawerProps> = ({ show, hide_menu }) => {
+
+    const [expandAccordion, setExpandAccordion] = useState(false)
+
     return (
         <div className={`sidedrawer ${show ? 'show' : ''}`}>
             <button
@@ -17,7 +22,25 @@ const Sidedrawer: FC<siderdrawerProps> = ({ show, hide_menu }) => {
             >
                 <IoIosClose />
             </button>
+            <ul>
+                <Link to="/"><li>Home</li></Link>
+                <Link to="/dashboard"><li>Dashboard</li></Link>
+                <div className="sidedrawer__accordion">
+                    <div className="sidedrawer__header"
+                        onClick={() => setExpandAccordion(prevState => !prevState)}
+                    >
+                        <span>Add new entry <i className={`icon ${expandAccordion ? 'rotate' : ''}`}><IoIosArrowDown /></i></span>
+                    </div>
+                    <div
+                        className={`sidedrawer__expandable-links ${expandAccordion ? 'show' : ''}`}
 
+                    >
+                        <Link to="/workouts" onClick={() => hide_menu(false)}><li>workout</li></Link>
+                        <Link to="/nutrition" onClick={() => hide_menu(false)} ><li>nutrition</li></Link>
+                        <Link to="/weight" onClick={() => hide_menu(false)}><li>weight</li></Link>
+                    </div>
+                </div>
+            </ul>
         </div >
     )
 }
